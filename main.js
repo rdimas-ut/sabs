1102240273a46c6a1bb20d61cde36e0d162fa847
Ruben Dimas <rdimas@utexas.edu>
2020-11-29 23:08:47 -0600

    finished electron react

---
+++ main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  //win.loadFile('index.html')
+  //win.webContents.openDevTools()
+  win.loadURL('http://localhost:3000')
+}
+
+// function createWindow () {
+//   const win = new BrowserWindow ({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       nodeIntegration: true
+//     }
+//   })
+
+//   win.loadURL('https://google.com')
+// }
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
